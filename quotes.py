ef1ce22a622fce337932ffe5db4c8f7e0820dd51
Md Touseef <mdtouseef15@gmail.com>
2021-07-02 23:43:55 +0530

    Create quotes.py

---
+++ quotes.py
@@ -0,0 +1,27 @@
+import discord
+import os
+import requests
+import json
+
+client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+client.run(os.getenv('TOKEN'))
