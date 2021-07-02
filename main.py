b3b1ebb83b6cff691c8cf4243a0c63e28fec2ff3
Md Touseef <mdtouseef15@gmail.com>
2021-07-03 00:31:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,13 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    
-def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return(quote)
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('bicchu'):
-        await message.channel.send('Bol re')
-    if message.content.startswith('marja'):
-        await message.channel.send('tu marja')
-     if message.content.startswith('$inspire'):
-        quote = get_quote()
-        await message.channel.send(quote)
-        
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
 client.run(os.getenv('TOKEN'))
